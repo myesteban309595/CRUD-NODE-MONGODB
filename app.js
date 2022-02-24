f833c905d27f6444ceb6a00af942e613d456d4ac
myesteban309595 <maryoe_95@hotmail.com>
2022-02-23 23:03:38 -0500

    base de datos y CRUD completa

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const personaSchema = mongoose.Schema ({
     edad: Number,
     pais: String
 
-})
+},{versionKey : false})
 
 const personaModel = mongoose.model('personas', personaSchema);
 
@@ -34,4 +34,46 @@ const mostrar = async () => {
     console.log(personas);
 }
 
-mostrar();
+//* crear
+
+const crear = async ()=> {
+    const persona = new personaModel({
+        nombre : "casimiro",
+        edad: 45,
+        pais: "panama"
+    })
+
+    const resultado = await persona.save()
+    console.log(resultado)
+}
+
+//* editar
+
+const actualizar = async (id)=> {
+    const persona = await personaModel.updateOne({
+
+        _id : id
+    },
+    {
+        $set:{
+            nombre: "nombre modificado",
+            edad : 1,
+            pais : "pais modificado"
+        }
+    }
+    )
+}
+
+//* eliminar
+
+const eliminar = async (id)=> {
+    const persona = await personaModel.deleteOne({
+
+        _id : id
+    }
+    )
+}
+//mostrar();
+//crear();
+//actualizar('6216f6a5b756acfd7476af1e');
+eliminar('6216f6a5b756acfd7476af1d')
